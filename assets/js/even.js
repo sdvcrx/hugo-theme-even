8fc596cbb6ad2fc7ddd15d2060fb9ba628de1114
sdvcrx <memory.silentvoyage@gmail.com>
2021-04-21 21:25:52 +0800

    feat(layout): using markdown img render hook

--- assets/js/even.js
+++ assets/js/even.js
@@ -126,12 +126,6 @@ Even._initToc = function() {
 
 Even.fancybox = function() {
   if ($.fancybox) {
-    $('.post-content').each(function() {
-      $(this).find('img').each(function() {
-        $(this).wrap(`<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`);
-      });
-    });
-
     $('.fancybox').fancybox({
       selector: '.fancybox',
       protect: true,
