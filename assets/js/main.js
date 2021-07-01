a815a3c9e2a64b0c8c20b84de4569758b8e8bc66
sdvcrx <memory.silentvoyage@gmail.com>
2021-07-01 21:47:32 +0800

    feat(scripts): disable backToTop on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-  Even.backToTop();
   // css: (max-width: 800px)
   // only enable on mobile
   if (window.innerWidth <= 800 && window.URL_slideout) {
     Even.getScript(window.URL_slideout, Even.mobileNavbar);
+  } else {
+    Even.backToTop();
   }
   Even.toc();
   Even.fancybox();
